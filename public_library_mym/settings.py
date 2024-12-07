b62bc1b86e224e391b21f4fc50939a0a82464d70
Hasan <hasan.dev04@gmail.com>
2024-12-07 13:14:31 +0600

    static root was set

--- public_library_mym/settings.py
+++ public_library_mym/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 
 env = environ.Env()
 environ.Env.read_env()
@@ -134,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "/media/"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
