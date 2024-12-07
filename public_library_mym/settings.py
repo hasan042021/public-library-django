b939364d08cfdb8c44c451b5a8a6c9cfe1d884b1
Hasan <hasan.dev04@gmail.com>
2024-12-07 12:51:32 +0600

    configured for vercel deployment

--- public_library_mym/settings.py
+++ public_library_mym/settings.py
@@ -91,10 +91,11 @@ WSGI_APPLICATION = "public_library_mym.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": env.db("DB_URL")
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
 }
 
 
--- public_library_mym/wsgi.py
+++ public_library_mym/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'public_library_mym.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "public_library_mym.settings")
 
 application = get_wsgi_application()
+
+app = application
