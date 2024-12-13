c0a1e852e0d25abd2e58d6b0595daa05ba3f2eb8
Hasan <hasan.dev04@gmail.com>
2024-12-13 23:23:45 +0600

    minor updates

--- public_library_mym/settings.py
+++ public_library_mym/settings.py
@@ -35,9 +35,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://*.127.0.0.1",
 ]
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500/index.html",
-]
+
 
 # Application definition
 
